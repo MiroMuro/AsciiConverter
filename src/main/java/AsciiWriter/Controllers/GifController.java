53aa9cf787f46400a38a2e45c0b8b4222571c647
MiroMuro <miro.laukkanen44@gmail.com>
2024-03-20 15:53:17 +0200

    Refactoring gif painting methods

--- src/main/java/AsciiWriter/Controllers/GifController.java
+++ src/main/java/AsciiWriter/Controllers/GifController.java
@@ -51,22 +51,13 @@ public class GifController {
 		playbackSpeedSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
 			System.out.println("Playback Speed Slider value changed: " + newValue);
 		});
+		// Add a listener to the font size slider.
 		fontSizeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
 			gifTextArea.setStyle("-fx-font-size: " + newValue + "px;");
 			System.out.println(gifTextArea.getStyle());
 			System.out.println("Font Size Slider value changed: " + newValue);
 		});
 
-		// 1000 / 50 = 20 frames per second
-		// 1000 / 25 = 40 frames per second
-		// 1000 / 100 = 10 frames per second
-		// 1000 / 200 = 5 frames per second
-		// 1000 / 300 = 3.33 frames per second
-		// Thread.sleep(300); = 3.33 frames per second.
-		// Thread.sleep(200); = 5 frames per second.
-		// Thread.sleep(100); = 10 frames per second.
-		// Thread.sleep(50); = 20 frames per second.
-		// Thread.sleep(25); = 40 frames per second.
 	}
 
 	@FXML
@@ -76,68 +67,70 @@ public class GifController {
 	}
 	public void displayGif(Image gif, String gifName) throws InterruptedException {
 		gifView.setImage(gif);
-		Writer.writeAsciiGifToFile(gifName);
 		System.out.println("Gif Path: " + gifName);
 		fileName = gifName;
 
 	}
-
 	@FXML
-	public void startPainting() throws InterruptedException {
-
+	public void paintGifToAscii(ActionEvent event) throws InterruptedException {
+		flag = true;
 		Task<Void> task = new Task<Void>() {
 			@Override
 			protected Void call() throws Exception {
-				flag = true;
 				GifDecoder gifDecoder = new GifDecoder();
 				gifDecoder.read("src/main/resources/gifs/" + fileName);
-				int n = gifDecoder.getFrameCount();
+				int frameCount = gifDecoder.getFrameCount();
 				int playbackSpeed = (int) playbackSpeedSlider.getValue();
 				int verticalDensity = (int) verticalDensitySlider.getValue();
 				int horizontalDensity = (int) horizontalDensitySlider.getValue();
+
 				while (flag) {
-					frameIndex = 0;
-					while (frameIndex < n - 1) {
-						System.out.println(frameIndex + " " + n);
-						Platform.runLater(new Runnable() {
-							@Override
-							public void run() {
-								BufferedImage frame = gifDecoder.getFrame(frameIndex);
-								Writer.writeAsciiImageToFileNoOpeningPropmt(frame, verticalDensity, horizontalDensity);
-								try {
-									File myObj = new File("src/main/resources/myFile.txt");
-									Scanner myReader = new Scanner(myObj);
-
-									while (myReader.hasNextLine()) {
-										String data = myReader.nextLine();
-										gifTextArea.appendText(data + "\n");
-									}
-									myReader.close();
-
-								} catch (FileNotFoundException e) {
-									System.out.println("An error occurred.");
-									e.printStackTrace();
-								}
-								frameIndex++;
-
-							}
-						});
+					for (frameIndex = 0; frameIndex < frameCount && flag; frameIndex++) {
+						BufferedImage gifFrame = gifDecoder.getFrame(frameIndex);
+						renderGifFrameAsAscii(gifFrame, verticalDensity, horizontalDensity);
 						Thread.sleep(playbackSpeed);
-						Platform.runLater(new Runnable() {
-							@Override
-							public void run() {
-								gifTextArea.clear();
-							}
-						});
+						clearGifTextArea();
 					}
 				}
-
-
 				return null;
 			}
 
 		};
+
 		new Thread(task).start();
+	}
+
+	private void clearGifTextArea() {
+		// Gui updates must be handled in the JavaFX Application Thread.
+		// Platform.runLater uses the JavaFX Application Thread.
+		Platform.runLater(gifTextArea::clear);
+	}
+
+	private void renderGifFrameAsAscii(BufferedImage gifFrame, int verticalDensity, int horizontalDensity) {
+		Platform.runLater(() -> {
+			// Write the current gif frame as ascii to the file.
+			Writer.writeAsciiImageToFileNoOpeningPropmt(gifFrame, verticalDensity, horizontalDensity);
+			appendAsciiFrameFromFile();
+		});
+
+	}
+
+	private void appendAsciiFrameFromFile() {
+		// Read the current gif ascii frame from the file and append it to the text
+		// area.
+		try {
+			File myObj = new File("src/main/resources/myFile.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				gifTextArea.appendText(data + "\n");
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred. File was not found.");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 
 	};
@@ -145,9 +138,9 @@ public class GifController {
 	@FXML
 	public void clickGifButton(ActionEvent event) throws InterruptedException {
 		flag = true;
-		while (flag) {
-			startPainting();
-		}
+		// startPainting();
+		paintGifToAscii(event);
+
 	}
 
 	@FXML
@@ -155,4 +148,54 @@ public class GifController {
 		flag = false;
 
 	}
+	/*
+	 * @FXML public void startPainting() throws InterruptedException { // Create a
+	 * new Task (that runs on separate thread from the JavaFX) to handle // the gif
+	 * painting. // The GUI must be updated in the JavaFX Application Thread, and //
+	 * In this case, that thread is accessed through the Platform.runLater method.
+	 * Task<Void> task = new Task<Void>() { // The interval between frame rendering
+	 * is handled by // Thread.sleep(playbackSpeed). // But we cannot sleep the
+	 * JavaFX Application Thread or else the app will // freeze, so we use a Task //
+	 * to encapsulate the Platform.runLater method, and sleep the Task thread //
+	 * instead. This ensures that the gif renders, unrenders and the GUI is updated
+	 * // at the same time.
+	 *
+	 * @Override protected Void call() throws Exception { flag = true; GifDecoder
+	 * gifDecoder = new GifDecoder(); gifDecoder.read("src/main/resources/gifs/" +
+	 * fileName); int n = gifDecoder.getFrameCount(); int playbackSpeed = (int)
+	 * playbackSpeedSlider.getValue(); int verticalDensity = (int)
+	 * verticalDensitySlider.getValue(); int horizontalDensity = (int)
+	 * horizontalDensitySlider.getValue(); while (flag) { frameIndex = 0; // This
+	 * while loop writes the gif as ascii art to the gifTextArea. while (frameIndex
+	 * < n - 1) { System.out.println(frameIndex + " " + n); // This updates the GUI
+	 * in the JavaFX Application Thread. Platform.runLater(new Runnable() {
+	 *
+	 * @Override public void run() { BufferedImage frame =
+	 * gifDecoder.getFrame(frameIndex);
+	 * Writer.writeAsciiImageToFileNoOpeningPropmt(frame, verticalDensity,
+	 * horizontalDensity); try { File myObj = new
+	 * File("src/main/resources/myFile.txt"); Scanner myReader = new Scanner(myObj);
+	 *
+	 * while (myReader.hasNextLine()) { String data = myReader.nextLine();
+	 * gifTextArea.appendText(data + "\n"); } myReader.close();
+	 *
+	 * } catch (FileNotFoundException e) { System.out.println("An error occurred.");
+	 * e.printStackTrace(); } frameIndex++;
+	 *
+	 * } }); // This sleeps the Task thread, not the JavaFX Application Thread.
+	 * Thread.sleep(playbackSpeed); // Clear the screen for the next frame. As an
+	 * GUI update must be handled in the // JavaFX Application Thread, which
+	 * PlatForm.runLater uses. Platform.runLater(new Runnable() {
+	 *
+	 * @Override public void run() { gifTextArea.clear(); } }); } }
+	 *
+	 *
+	 * return null; }
+	 *
+	 * }; new Thread(task).start();
+	 *
+	 *
+	 * };
+	 */
+
 }
